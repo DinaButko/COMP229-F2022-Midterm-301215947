5c380827c81613b54449a66e02c44f260ff30f3d
DzinaButko <butkodina@gmail.com>
2022-02-28 14:01:52 -0500

    files from folder

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 ## Welcome to the MidTerm Project - the Favourite Book List App
 
 please use **`npm install`** to install project dependencies
-test
--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
+
 module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://127.0.0.1/books_store"
   "URI": "mongodb+srv://Dina:ntPkyV4HRnmpRPGu@mongodbserver.fagip.mongodb.net/book_store?retryWrites=true&w=majority"
-}
+ }
+ 
