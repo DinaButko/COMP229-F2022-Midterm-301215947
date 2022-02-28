d9fadb7c20246a29103a1479f85a139470ea07c4
DzinaButko <butkodina@gmail.com>
2022-02-28 12:29:32 -0500

    add dependencies

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
-};
+  //"URI": "mongodb://127.0.0.1/books_store"
+  "URI": "mongodb+srv://Dina:ntPkyV4HRnmpRPGu@mongodbserver.fagip.mongodb.net/book_store?retryWrites=true&w=majority"
+}
